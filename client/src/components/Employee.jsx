462f0a59d6bef70c0b9d70433c0a091fd6c99973
Jonathan <jonathan.y.cai@outlook.com>
2024-08-23 14:38:01 -0700

    Implemented data fetching for Employee page

--- client/src/components/Employee.jsx
+++ client/src/components/Employee.jsx
@@ -1,3 +1,14 @@
-export function Employee() {
+import { useParams } from "react-router";
+
+function Employee() {
+    const { id } = useParams();
+    const { data } = useQuery(["employee", id], async () => {
+        const employeeData = await fetch(`http://localhost:3000/employees/${id}`);
+        return employeeData.json();
+    });
+
+    console.log("employeeData", data);
 
 }
+
+export default Employee;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,15 +1,21 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 import App from "./App";
 import theme from "./theme";
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
       <BrowserRouter>
-        <App />
+        <QueryClientProvider client={queryClient}>
+          <App />
+        </QueryClientProvider>
       </BrowserRouter>
     </ChakraProvider>
   </React.StrictMode>
