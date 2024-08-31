bc795e206fe5cca2ddde29b034106bb62c8b9003
Jonathan <jonathan.y.cai@outlook.com>
2024-08-31 04:00:24 -0700

    Test Employee page using MSW for both badge scenarios:
    * Added test for employee with badges to verify correct rendering.
    * Added test for employee without badges to ensure proper handling of empty states.

--- client/src/__tests__/test-utils.jsx
+++ client/src/__tests__/test-utils.jsx
@@ -1,11 +1,15 @@
+import { QueryClientProvider } from "@tanstack/react-query";
 import { render } from "@testing-library/react";
 import React from "react";
 import { MemoryRouter } from "react-router-dom";
+import { queryClient } from "../query-client";
 
 const AllTheProviders = ({ children }) => {
     return (
         <React.StrictMode>
-            <MemoryRouter>{children}</MemoryRouter>
+            <MemoryRouter>
+                <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+            </MemoryRouter>
         </React.StrictMode>
     )
 }
